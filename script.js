867f68622244c960071cf5483f52bbaecf6c6c7c
Stolyar100 <olejer555@gmail.com>
2020-09-02 23:12:21 +0300

    added on click autoComplete function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-let sdate = document.getElementById('sdate')
-let edate = document.getElementById('edate')
+let sdate = document.getElementById('sdate').nextElementSibling
+let edate = document.getElementById('edate').nextElementSibling
 let buttonsBlock = sdate.parentNode.parentNode.parentNode.parentNode.lastChild
 
 buttonsBlock.style.display = 'flex'
 buttonsBlock.style.justifyContent = 'space-between'
 
+const autoComplete = (e) => {
+    e.preventDefault()
+
+    let currentDate = new Date
+    let [currentYear, currentMonth, currentDay] = [currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()]
+    let nextDate = new Date(currentYear, currentMonth, currentDay + 30)
+    let [nextYear, nextMonth, nextDay] = [nextDate.getFullYear(), nextDate.getMonth(), nextDate.getDate()]
+
+    sdate.value = `${currentDay}.${currentMonth + 1}.${currentYear}`
+    edate.value = `${nextDay}.${nextMonth + 1}.${nextYear}`
+}
+
 let autoCompleteButton = document.createElement('button')
 autoCompleteButton.textContent = 'Автоматично на місяць'
 autoCompleteButton.className = 'btn btn-primary'
+autoCompleteButton.addEventListener('click', autoComplete)
 buttonsBlock.prepend(autoCompleteButton)
